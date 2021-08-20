51d6b207908551b12d60b685a9da0ea289db8196
tranuongkyoto <tranduongkyoto@gmail.com>
2021-08-20 11:07:58 +0700

    edit css

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -16,3 +16,22 @@ iconTogle.onclick = function () {
 function onClickTogleMenu() {
   menuMobile.classList.toggle('clicked');
 }
+
+const feedback = [0, 1, 2];
+let i = 0;
+const onChange = (value) => {
+  if (value) i = i + 1;
+  else i = i - 1;
+  i = i % 3 < 0 ? 2 : i % 3;
+  const rb = document.getElementsByClassName('review-border');
+  const fp = document.getElementsByClassName('feedback-pagi');
+  for (let y = 0; y < feedback.length; y++) {
+    rb[y].classList = 'review-border';
+    fp[y].classList = 'feedback-pagi';
+  }
+  rb[i].classList = 'review-border active';
+  fp[i].classList = 'feedback-pagi active';
+};
+setInterval(() => {
+  onChange(true);
+}, 5000);
